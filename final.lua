8ba06a2980edd9e0be0bab61c84f535b6979bb1f
ItsIron135 <140293975+ItsIron135@users.noreply.github.com>
2026-04-02 21:15:19 -0600

    Update final.lua

--- final.lua
+++ final.lua
@@ -5,7 +5,7 @@ local player = Players.LocalPlayer
 local pGui = player:WaitForChild("PlayerGui")
 
 -- FLAGS
-local UI_NAME = "RocketAdmin_V72"
+local UI_NAME = "RocketAdmin"
 local isLooping = false
 local isStackingActive = false
 local isGodMode = false 
@@ -145,7 +145,7 @@ RunService.Heartbeat:Connect(function()
     end
 end)
 
--- 5. 40MS TACTICAL CYCLE
+-- 5. 20MS TACTICAL CYCLE
 task.spawn(function()
     while true do
         local char = player.Character
@@ -161,7 +161,7 @@ task.spawn(function()
                 local tool = jumpers[i]
                 if tool.Parent ~= char then tool.Parent = char end
                 tool:Activate()
-                task.wait(0.04) 
+                task.wait(0.02) 
                 if not isStackingActive and bp then tool.Parent = bp end
             end
         end
