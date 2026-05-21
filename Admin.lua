2b50e66f43c8f790fecbbb7d6dda2b0e1338c725
ItsIron135 <140293975+ItsIron135@users.noreply.github.com>
2026-05-20 17:39:45 -0700

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -40,9 +40,9 @@ local laserNames = {Rain=1,Beam=1,RainBeam=1,Effect=1,Center=1,Part=1,StarShard=
 local homeCFrame = CFrame.new(0, 10, 0)
 
 local giveAllRunning = false
-local GIVE_ALL_NORMAL_DELAY = 0
+local GIVE_ALL_NORMAL_DELAY = 0.02
 local GIVE_ALL_BURST_TIME = 0.1
-local GIVE_ALL_BURST_AMOUNT = 120
+local GIVE_ALL_BURST_AMOUNT = 200
 local GIVE_ALL_REMOTES = {"SpawnRainbowBlock","SpawnDiamondBlock","SpawnSuperBlock","SpawnLuckyBlock","SpawnGalaxyBlock"}
 
 ---------------------------------------------------------
