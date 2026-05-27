aecbaa785ec7d92bee134175a6ef719e7098e43d
ItsIron135 <140293975+ItsIron135@users.noreply.github.com>
2026-05-26 22:39:47 -0600

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -466,7 +466,7 @@ end
 ---------------------------------------------------------
 -- ROCKET SPAM & DESYNC
 ---------------------------------------------------------
-local ROCKET_MULTIPLIER = 1 -- How many times to fire PER launcher, PER frame. (Increase if you want it even faster!)
+local ROCKET_MULTIPLIER = 2 -- How many times to fire PER launcher, PER frame. (Increase if you want it even faster!)
 
 local function fireRocketAt(pos)
     local char, bp = LP.Character, LP:FindFirstChild("Backpack")
